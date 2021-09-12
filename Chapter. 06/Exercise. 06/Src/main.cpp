9246cf2f08a01d73983c00544f0c0b84b81bdb92
sheer-rey <sheer.rey@gmail.com>
2021-09-12 20:10:55 +0800

    setup a new Chapter's excercise (Exercise. 06)

---
+++ Chapter. 06/Exercise. 06/Src/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <array>
+#include <string>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    //Subject 01
+    cout << "Subject 01" << endl;
+    cout << "Please enter two integer: ";
+
+    cout << "\nPress any key to continue." << endl;
+    cin.get();
+
+    return 0;
+}
