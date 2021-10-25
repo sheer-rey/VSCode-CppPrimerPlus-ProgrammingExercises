51d5752bea7b4e33356497cb08abb662997e2db7
sheer-rey <sheer.rey@gmail.com>
2021-10-25 23:39:42 +0800

    Set up new Exercise. 08

---
+++ Chapter. 08/Exercise. 08/Src/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // Subject 01
+    cout << "Subject 01" << endl;
+
+    cin.sync();
+    cout << "\nPress any key to continue." << endl;
+    cin.get();
+
+    return 0;
+}
